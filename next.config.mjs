9eba9f64e9983a43e3a789b36ad9ecae1ea1c57e
adam ives <hex.zeus@yahoo.com>
2024-08-08 22:16:33 -0400

    Update application

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,12 @@
-// next.config.mjs
-
 const ContentSecurityPolicy = `
   default-src 'self';
   script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com https://m.stripe.network;
   style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
   img-src 'self' data: https://files.cdn.printful.com;
-  connect-src 'self' ws://localhost:3001 https://api.stripe.com https://m.stripe.network;
-  font-src 'self' https://fonts.gstatic.com;
+  connect-src 'self' ws://localhost:3000 https://api.stripe.com https://m.stripe.network;
+  font-src 'self' https://fonts.gstatic.com https://fonts.googleapis.com;
   frame-src https://js.stripe.com;
+  worker-src 'self' blob:;
 `;
 
 const securityHeaders = [
@@ -15,7 +14,6 @@ const securityHeaders = [
         key: 'Content-Security-Policy',
         value: ContentSecurityPolicy.replace(/\n/g, ''),
     },
-    // You might want to add other security headers here, such as:
     {
         key: 'X-XSS-Protection',
         value: '1; mode=block'
@@ -42,6 +40,15 @@ const nextConfig = {
                 headers: securityHeaders,
             },
         ];
+    },
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.resolve.fallback = {
+                ...config.resolve.fallback,
+                fs: false,
+            };
+        }
+        return config;
     },
 };
 
